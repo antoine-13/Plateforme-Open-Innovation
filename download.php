45dbc454cc9029e19a3c2f14bc08dea69a35dd2b
antoine-13 <62340674+antoine-13@users.noreply.github.com>
2020-06-11 14:13:58 +0200

    Delete download.php

--- download.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $req = $db->prepare("SELECT fichier_description FROM description WHERE id_projet = ?");
-    $exec = $req->execute(array($_GET['id']));
-    $result = $req->fetchAll();
-    $url_fichier = $result[0]['fichier_description'];
-?>
